c62e2c2dc35cdfb21ede663fa501f539276919f5
maggiegraves1995 <33082547+maggiegraves1995@users.noreply.github.com>
2017-10-30 19:47:52 -0700

    Update AccountTest.java

--- AccountTest.java
+++ AccountTest.java
@@ -10,26 +10,26 @@ public class AccountTest {
 		Scanner input = new Scanner(System.in);	
 		
 		int transactionCode;
-        int num_Of_Acct; //number of accounts
-		Account acct, acct1 = null, acct2 = null, acct3 = null, acct4 = null, acct5 = null;
+       		int num_Of_Acct; //number of accounts
 		System.out.println("enter number of accounts");
 		num_Of_Acct = input.nextInt();
 		
-		for (int i=1; i<=num_Of_Acct; i++) {
+		for (int counter = 0; counter < num_Of_Acct; counter++) {
 			//prompt to enter first name, last name, , account number, 
 			//credit limit, and beg balance
 			System.out.printf("%n%nFor account %d, enter: first name, last name, account number, "
 					+ "credit limit, and beg balance %n ", i);	
 			String firstName = input.next(); 
 			String lastName = input.next();
+			customers.add(new Customer(firstName, lastName);
 			int accountNumber = input.nextInt();
 			double creditLimit = input.nextDouble();
 			double balance = input.nextDouble();			
 			
-			acct = new Account (firstName, lastName, accountNumber);
+			accts.add(new Account (accountNumber);
 					
-			acct.setBegBalance(balance);			
-			acct.setCreditLimit(creditLimit);
+			accts.setBegBalance(balance);			
+			accts.setCreditLimit(creditLimit);
 			
 			//prompt for all transactions for current account
 			System.out.println();
@@ -42,46 +42,36 @@ public class AccountTest {
 				if (transactionCode == 1) {					
 					double costOfPurchase = input.nextDouble();					
 					double payment = input.nextDouble();
-					acct.processTransaction(costOfPurchase, payment);
+					accts.processTransaction(costOfPurchase, payment);
 					
 					//
 				}
 				// payment only.  input amount and process
 				else if (transactionCode == 2) {
 					double payment = input.nextDouble();
-					acct.processTransaction(payment);
+					accts.processTransaction(payment);
 				}
 				// input next transaction code
 				transactionCode = input.nextInt();
 			}//end of while loop
 				
-			//assign the values of the reference variable "acct" to the 
-			//appropriate account objects
-			if (i==1)
-				acct1 = acct;
-			else if (i==2)
-				acct2 = acct;
-			else if (i==3)
-				acct3 = acct;
-			else if (i==4)
-				acct4 = acct;
-			else if (i==5)
-				acct5 = acct;
-		
-			System.out.println();
-				
+			
 		}//end of for loop
 		
 		//display an information summary for each account object
 		System.out.println("\n\n ****************************************************"
 				+ "\nSummary of the accounts \n");
+		for (Account anAccount: accts)
+		{			  
 		
-		acct1.printStatement();
-		acct2.printStatement();
-		acct3.printStatement();
-		acct4.printStatement();
-		acct5.printStatement();
-		
+			System.out.printf("Name: %s %s\n Account number: %d \nCredit limit: %.2f%n"
+		            + " Beginning Balance: %.2f   Ending Balance: %.2f\n Number of "
+					+ "purchases: %d\nNumber of transactions: %d\n Total Purchase Ammount: %.2f" 
+		            + "  Total Payment Amount: %.2f  Total Penalty: %.2f \n\n", 
+					anAccount.getOwner(), anAccount.getAccountNumber(), anAccount.getCreditLimit(), anAccount.getBegBalance(), 
+					anAccount.getEndBalance(), anAccount.getPurchases(), anAccount.getTransactions(), anAccount.getTotPurchaseAmount(),anAccount.getTotPayment(), anAccount.getTotPenalty();
+		}
+			
 	}//end of main method	
 }// end of class AccountTest
 
