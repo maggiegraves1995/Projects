9973cc51a613ae259319227a279975f727d7756d
maggiegraves1995 <33082547+maggiegraves1995@users.noreply.github.com>
2017-10-30 18:01:13 -0700

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,22 @@
+public class Customer {
+	
+	//instance variables
+	private String firstName, lastName;
+	
+	public Customer(String firstName, String lastName)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+				
+	}
+	public String getFirstName() 
+	{
+		return firstName; 
+	} 
+    //method returns the last name
+	public String getLastName() 
+    {
+		return lastName; 
+	} 
+
+}
