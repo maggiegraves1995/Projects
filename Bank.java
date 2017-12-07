689ba7e492d62360138360b0002ec81c9612629c
maggiegraves1995 <33082547+maggiegraves1995@users.noreply.github.com>
2017-12-07 10:10:37 -0800

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -14,8 +14,7 @@ class Bank
     private JFrame addNewAccountFrame;
     private JFrame processPaymentFrame;
     private JFrame processPurchaseFrame;
-
- 
+    
  public Bank()
     {
         accounts = new ArrayList<Account>();
@@ -58,50 +57,54 @@ class Bank
             chooseOperationFrame.setVisible(true);
         }
         //
-       
- 
  // Add New Account
         {
             addNewAccountFrame = new JFrame("Add Account Information");
             addNewAccountFrame.setLayout(new FlowLayout());
             addNewAccountFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            
+            JLabel fNameLabel = new JLabel("First Name:");
+            final JTextField fNameField = new JTextField(30);
+            fNameLabel.setLabelFor(fNameField);
+            addNewAccountFrame.add(fNameLabel);
+            addNewAccountFrame.add(fNameField);
+            
             JLabel nameLabel = new JLabel("Last Name:");
             addNewAccountFrame.add(nameLabel);
-            JTextField nameField = new JTextField(30);
+            final JTextField nameField = new JTextField(30);
+            nameLabel.setLabelFor(nameField);
             addNewAccountFrame.add(nameField);
 
             JLabel accountNumLabel = new JLabel("Account Number:");
             addNewAccountFrame.add(accountNumLabel);
-            JTextField accountNumField = new JTextField(30);
+            final JTextField accountNumField = new JTextField(30);
+            accountNumLabel.setLabelFor(accountNumField);
             addNewAccountFrame.add(accountNumField);
 
             JLabel balanceLabel = new JLabel("Initial balance:");
             addNewAccountFrame.add(balanceLabel);
-            JTextField balanceField = new JTextField(10);
+            final JTextField balanceField = new JTextField(10);
+            balanceLabel.setLabelFor(balanceField);
             addNewAccountFrame.add(balanceField);
             
             JLabel creditLimitLabel = new JLabel("Credit Limit:");
             addNewAccountFrame.add(creditLimitLabel);
-            JTextField creditLimitField = new JTextField(10);
+            final JTextField creditLimitField = new JTextField(10);
+            creditLimitLabel.setLabelFor(creditLimitField);
             addNewAccountFrame.add(creditLimitField);
 
             JButton submitButton = new JButton("Submit");
             ActionListener handler = new ActionListener() {
                 public void actionPerformed(ActionEvent event) {
                     //System.out.println("You pressed: " + event.getActionCommand());
-                    attemptAddAccount();
+                    attemptAddAccount(fNameField, nameField, accountNumField, balanceField, creditLimitField);
                 }
             };
             submitButton.addActionListener(handler);
             addNewAccountFrame.add(submitButton);
-
             addNewAccountFrame.setSize(400, 200);
             addNewAccountFrame.setVisible(false);
         }
-
-        //
-       
- 
  // Process Purchase
         {
             processPurchaseFrame = new JFrame("Process a Purchase");
@@ -111,24 +114,27 @@ class Bank
             processPurchaseFrame.setVisible(false);
             JLabel acctNumLabel = new JLabel("Account Number:");
             processPurchaseFrame.add(acctNumLabel);
-            JTextField acctNumField = new JTextField(30);
+            final JTextField acctNumField = new JTextField(30);
+            acctNumLabel.setLabelFor(acctNumField);
             processPurchaseFrame.add(acctNumField);
 
             JLabel paymentLabel = new JLabel("Payment Amount:");
             processPurchaseFrame.add(paymentLabel);
-            JTextField paymentField = new JTextField(30);
+            final JTextField paymentField = new JTextField(30);
+            paymentLabel.setLabelFor(paymentField);
             processPurchaseFrame.add(paymentField);
             
             JLabel purchaseLabel = new JLabel("Cost of Purchase:");
             processPurchaseFrame.add(purchaseLabel);
-            JTextField purchaseField = new JTextField(30);
+            final JTextField purchaseField = new JTextField(30);
+            purchaseLabel.setLabelFor(purchaseField);
             processPurchaseFrame.add(purchaseField);   
             
             JButton submitButton = new JButton("Submit");
             ActionListener handler = new ActionListener() {
                 public void actionPerformed(ActionEvent event) {
                     //System.out.println("You pressed: " + event.getActionCommand());
-                    attemptProcessPurchase();
+                    attemptProcessPurchase(acctNumField, paymentField, purchaseField);
                 }
             };
             submitButton.addActionListener(handler);
@@ -147,19 +153,21 @@ class Bank
             
             JLabel acctNum = new JLabel("Account Number:");
             processPaymentFrame.add(acctNum);
-            JTextField acctNumField = new JTextField(30);
+            final JTextField acctNumField = new JTextField(30);
+            acctNum.setLabelFor(acctNumField);
             processPaymentFrame.add(acctNumField);
             
             JLabel paymentLabel = new JLabel("Payment Amount:");
             processPaymentFrame.add(paymentLabel);
-            JTextField paymentField = new JTextField(30);
+            final JTextField paymentField = new JTextField(30);
+            paymentLabel.setLabelFor(paymentField);
             processPaymentFrame.add(paymentField);
             
             JButton submitButton = new JButton("Submit");
             ActionListener handler = new ActionListener() {
                 public void actionPerformed(ActionEvent event) {
                     //System.out.println("You pressed: " + event.getActionCommand());
-                    attemptProcessPayment();
+                    attemptProcessPayment(acctNumField, paymentField);
                 }
             };
             submitButton.addActionListener(handler);
@@ -188,27 +196,53 @@ class Bank
         chooseOperationFrame.setVisible(false);
         processPaymentFrame.setVisible(true); 
     }
-    private void attemptAddAccount()
+    private void attemptAddAccount(JTextField fNameField, JTextField nameField, JTextField accountNumField, JTextField balanceField, JTextField creditLimitField)
     {
-        boolean isValidInput = false;
-        System.out.println("TODO: verify that all fields are filled out & valid");
-        System.out.println("TODO: verify that account with specified number doesn't already exist");
-
-        if (isValidInput) {
-            System.out.println("TODO: create new account");
-        } else {
-
-            JOptionPane.showMessageDialog(null, "Please correct your inputs. Make sure fields are are filled out and account doesn't already exist", "Illegal Input", JOptionPane.ERROR_MESSAGE);
-
-        }
+    
+      boolean isValidInput = false; 
+      String firstName = fNameField.getText();
+      String lastName = nameField.getText();
+      Double accountNumber = Double.parseDouble(accountNumField.getText());
+      Double balance = Double.parseDouble(balanceField.getText());
+      Double creditLimit = Double.parseDouble(creditLimitField.getText());
+      
+      Account account = new Account(firstName, lastName, accountNumber);
+		
+		account.setBegBalance(balance);			
+		account.setCreditLimit(creditLimit);
+		accounts.add(account);
+		String accountDisplay = account.getFirstName() + " " + account.getLastName() + " "
+				+"account Number"+ " " + account.getAccountNumber()+" "
+				+"begin balance:"+ account.getBegBalance() + "credit limit:"
+				+ account.getCreditLimit();
+		
+		JOptionPane.showMessageDialog(null,"Account Created"+ accountDisplay);
+		
     }
-    private void attemptProcessPurchase()
+    private void attemptProcessPurchase(JTextField acctNumField, JTextField paymentField, JTextField purchaseField)
     {
+    	boolean isValidInput = false;
+    	Double accountNumber = Double.parseDouble(acctNumField.getText());
+    	Double payment = Double.parseDouble(paymentField.getText());
+    	Double costOfPurchase = Double.parseDouble(purchaseField.getText());
+    	for(Account acct: accounts)    		
+    		acct.transactionOne(costOfPurchase, payment);
+		
+		JOptionPane.showMessageDialog(null,"Account Created"+ accountDisplay);
+		
+
     	
+    	 	
     }
-    private void attemptProcessPayment()
-    {
+    private void attemptProcessPayment(JTextField acctNumField, JTextField paymentField)
+    {	
+    	Double accountNumber = Double.parseDouble(acctNumField.getText());
+    	Double payment = Double.parseDouble(paymentField.getText());
+    	for(Account acct : accounts)
+    		if accounts.contains(accountNumber)
     	
+    		
     }
     
 }
+
